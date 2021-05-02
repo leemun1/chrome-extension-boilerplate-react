2c1b33f1241488643bc5ecbb3a5d81f9ebd79542
Mike Lee <mikemunkyu.lee@gmail.com>
2021-05-02 16:15:50 -0400

    feat: add button to reaload runtime on dev

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Greetings from '../../containers/Greetings/Greetings';
+import Reloader from '../../containers/Reloader/Reloader';
 import './Popup.css';
 
 const Popup = () => {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      {process.env.NODE_ENV === 'development' && <Reloader />}
+      <header className='App-header'>
         <Greetings />
       </header>
     </div>
